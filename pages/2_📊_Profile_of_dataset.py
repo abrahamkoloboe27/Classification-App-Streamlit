c52124f6a8d408eeef18bce515a77f84ede52022
Abraham KOLOBOE <114146395+abrahamkoloboe27@users.noreply.github.com>
2024-06-18 17:49:53 +0100

    Delete Profile of data set

--- pages/2_📊_Profile_of_dataset.py
+++ pages/2_📊_Profile_of_dataset.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import pandas as pd
-from streamlit_pandas_profiling import st_profile_report
+
 st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded")
