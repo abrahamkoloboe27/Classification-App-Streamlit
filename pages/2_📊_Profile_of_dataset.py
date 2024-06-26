c13e53fabb0ac4a78ba11589eedd5d4b891f7bbd
Abraham KOLOBOE <114146395+abrahamkoloboe27@users.noreply.github.com>
2024-06-26 15:42:02 +0100

    Try add Streamlit Report

--- pages/2_📊_Profile_of_dataset.py
+++ pages/2_📊_Profile_of_dataset.py
@@ -1,7 +1,20 @@
 import streamlit as st
+import pandas as pd
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
+
 
 st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded")
 st.header("Profile Dataset",divider='rainbow')
-st.warning("Cela ne fonctionne pas pour le moment, veuillez réessayer plus tard !")
+if "df" not in st.session_state :
+  st.session_state.df = None
+st.title("Profile Report")
+df = st.session_state.df 
+if df is not None : 
+  pr = df.profile_report()
+
+  st_profile_report(pr)
+else :
+  st.warning("Pas de données")
